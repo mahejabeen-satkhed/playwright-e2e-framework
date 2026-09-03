e21305bb9aa91057cf790392dce5acacb87bbded
Mahejabeen Satkhed <mahejabeen.satkhed@gmail.com>
2026-09-03 14:09:45 -0400

    Automated ADMIN-02

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -2,12 +2,25 @@ class AdminPage{
     constructor(page){
         this.page=page;
         this.adminHeader = page.locator('.oxd-text.oxd-text--h6.oxd-topbar-header-breadcrumb-module')
+        this.userMngmtDD = page.locator("//span[normalize-space()='User Management']")
+        this.userMngmtOptions = page.locator("//ul[@class='oxd-dropdown-menu']//a")
+        this.userRecords = page.locator("//div[@class='oxd-table-card']")
+
     }
 
     async goto(){
         await this.page.goto('/web/index.php/admin/viewSystemUsers');
     }
 
+    async clickUserManagementDropdown(){
+        await this.userMngmtDD.click();
+    }
+
+    async getUserRecordsCount(){
+        const usersCount = await this.userRecords.count();
+        return usersCount
+    }
+
 }
 
-module.exports = { AdminPage };
+module.exports = { AdminPage }; 
--- src/tests/admin.spec.js
+++ src/tests/admin.spec.js
@@ -22,3 +22,31 @@ test('Verify Admin page loads', async({},testInfo) => {
 
     console.log('Admin page loaded successfully');
 })
+
+//TC-ID : ADMIN-02
+test('Verify user mngmt section', async({}, testInfo) => {
+    const page = testInfo.page;
+    
+    const homePage = new HomePage(page);
+    await expect(homePage.welcomeMessage).toBeVisible();
+
+    await homePage.clickNavigationLink('Admin');
+
+    const adminPage = new AdminPage(page);
+    await expect(adminPage.adminHeader).toBeVisible();
+
+    console.log('Admin page loaded successfully');
+
+    await adminPage.clickUserManagementDropdown();
+
+    await adminPage.userMngmtOptions.first().click();
+
+    await page.waitForLoadState('networkidle');
+
+    const userCount = await adminPage.getUserRecordsCount();
+  
+    console.log(`Total user records: ${userCount}`);
+
+    expect(userCount).toBeGreaterThan(0);
+
+})
